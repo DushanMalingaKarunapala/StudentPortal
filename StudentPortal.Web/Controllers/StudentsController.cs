f15e1b7d3850542458d4aaf92df6753c0c20a250
K Dushan Malinga Karunapala <dushanforrest343@gmail.com>
2024-08-27 19:22:37 +0530

    Add project files.

---
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public StudentsController(ApplicationDbContext DbContext)
+        {
+            _dbContext = DbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public async Task <IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                name = viewModel.name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subcribed = viewModel.Subcribed,
+            };
+            await _dbContext.Students.AddAsync(student);
+            await _dbContext.SaveChangesAsync();
+            return View();
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> StudentList()
+        {
+            var  students = await _dbContext.Students.ToListAsync();
+
+            return View(students);
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await _dbContext.Students.FindAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await _dbContext.Students.FindAsync(viewModel.id);
+
+            if(student is not null) {
+                student.name = viewModel.name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;    
+                student.Subcribed = viewModel.Subcribed;
+
+                await _dbContext.SaveChangesAsync();
+                
+            }
+            return RedirectToAction("StudentList", "Students");
+        }   
+    }
+}
---
+++ StudentPortal.Web/Models/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentPortal.Web.Models.Entities
+{
+    public class Student
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Subcribed { get; set; }
+    }
+}
